746f415ebe835c60e0183b68f4bd435d67c57d8a
Tushit Garg <tushit.23@gmail.com>
2020-05-07 00:57:55 +0530

    Frog 2

--- Frog 2.cpp
+++ Frog 2.cpp
@@ -1,4 +1,4 @@
-// Problem B - Educational Dynamic Programming Contest - AtCoder
+// Educational Dynamic Programming Contest - AtCoder
 // Link: https://atcoder.jp/contests/dp/tasks/dp_b
 // B - Frog 2
 
